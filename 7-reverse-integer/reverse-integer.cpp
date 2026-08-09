6f964eea9ef71d5fce8de7954b53405bb90ff8b4
Aditya Sharma <108331223+Adittyyya@users.noreply.github.com>
2026-08-09 18:03:51 +0530

    Time: 2 ms (38.22%) | Memory: 8.4 MB (98.39%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int reverse(int n) {
         int revNum = 0;
-        while(n != 0){
+        while(n != 0){ //This will check from negative int to pov int val(as given in ques)
             int digit = n%10;
             //Imp edge case given in question
 
             if(revNum > INT_MAX/10 || revNum < INT_MIN/10){
                 return 0;
             }
-            
+
             revNum = (revNum*10) + digit;
             n = n/10;
         }
